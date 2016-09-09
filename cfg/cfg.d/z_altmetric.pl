7bce56f89e9af0771e5ec6053700889912db15ce
jesusbagpuss <j.salter@leeds.ac.uk>
2016-09-09 06:56:13 +0100

    Enable ISBN based Altmetrics
    Reworked data extraction from EPrint to allow for other IDs to be
    extracted if they exist. The function is now a config option in
    cfg/cfg.d/z_altmetrics.pl

--- cfg/cfg.d/z_altmetric.pl
+++ cfg/cfg.d/z_altmetric.pl
@@ -13,3 +13,25 @@ $c->{altmetric}->{base_url} = "http://api.altmetric.com/v1";
 # Optional API key - see http://api.altmetric.com/index.html#keys
 # $c->{altmetric}->{api_key} = "";
 
+# Function to return id type and id.
+# For supported id_types, check the Altmetrics API reference
+# Currently, this supports doi, isbn, arXivID, PMID, ads and uri.
+# If an Eprints has multiple usable identifiers, the first returned value will be used.
+$c->{altmetric}{get_type_and_id} = sub {
+        my( $eprint ) = @_;
+
+        if( $eprint->exists_and_set( "doi" ) ){
+                return( "doi", $eprint->value( "doi" ) );
+        }
+        if( $eprint->exists_and_set( "isbn" ) ){
+                return( "isbn", $eprint->value( "isbn" ) );
+        }
+
+		# id_numbers that have 10. in them (rudimentary doi check)
+        if( $eprint->exists_and_set( "id_number" ) && ( $eprint->value( "id_number" ) =~ /\b10./ ) ){
+		        
+                return( "doi", $eprint->value( "id_number" ) );	
+		}
+
+		#other fields could be checked and returned here.
+};
--- lib/plugins/EPrints/Plugin/Screen/EPrint/Box/Altmetric.pm
+++ lib/plugins/EPrints/Plugin/Screen/EPrint/Box/Altmetric.pm
@@ -9,9 +9,9 @@ sub can_be_viewed
         my( $self ) = @_;
 
         return 0 if $self->{session}->get_secure;
-
-        return 0 if( !defined $self->{processor}->{eprint} || !$self->{processor}->{eprint}->exists_and_set( 'id_number' ) );
-
+        return 0 if( !defined $self->{processor}->{eprint} );
+        return 0 if( !$self->{session}->can_call( "altmetric", "get_type_and_id" ) );
+		
         return 1;
 }
 
@@ -20,17 +20,20 @@ sub render
         my( $self ) = @_;
 
         my $session = $self->{session};
-        my $eprint = $self->{processor}->{eprint};
 
-	my $frag = $session->xml->create_document_fragment;
+        my $frag = $session->xml->create_document_fragment;
 
-        my $div = $frag->appendChild( $session->make_element( 'div', id => 'altmetric_summary_page', "data-doi" => $eprint->value( 'id_number' ) ) );
+        my ( $type, $id ) = $session->call( [ "altmetric", "get_type_and_id" ], $self->{processor}->{eprint} );
 
-	$frag->appendChild( $session->make_javascript( <<EOJ ) );
+        return $frag if ( !defined $type || !defined $id );
+		
+        my $div = $frag->appendChild( $session->make_element( 'div', id => 'altmetric_summary_page', "data-altmetric-id-type" => $type, "data-altmetric-id" => $id ) );
+
+		$frag->appendChild( $session->make_javascript( <<EOJ ) );
 new EP_Altmetric_Badge( 'altmetric_summary_page' );
 EOJ
 
-	return $frag;
+        return $frag;
 }
 
 1;
