f3a0c5c54716861b6e6da535fa5e08d793bf7ef7
John Salter <j.salter@leeds.ac.uk>
2022-08-10 12:31:21 +0100

    Fixes #4 - default to https API endpoint

--- cfg/cfg.d/z_altmetric.pl
+++ cfg/cfg.d/z_altmetric.pl
@@ -15,7 +15,7 @@ $c->{plugins}->{"Screen::EPrint::Box::Altmetric"}->{appears}->{summary_bottom} =
 $c->{plugins}->{"Screen::EPrint::Box::Altmetric"}->{appears}->{summary_right} = undef;
 
 # Altmetric API URL
-$c->{altmetric}->{base_url} = "http://api.altmetric.com/v1";
+$c->{altmetric}->{base_url} = "https://api.altmetric.com/v1";
 
 # Optional API key - see http://api.altmetric.com/index.html#keys
 # $c->{altmetric}->{api_key} = "";
